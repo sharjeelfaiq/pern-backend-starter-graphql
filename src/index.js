53d858a1fe798cd4506a5a814a7b34cb50da92f7
Sharjeel Faiq <mrmalik610@gmail.com>
2025-11-09 19:08:13 +0500

    Refactors application setup and removes morgan
    Simplifies application setup by removing the morgan middleware and console logs.
    This change centralizes the Apollo Server setup and removes request logging.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
 import express from "express";
-import { ApolloServer } from "@apollo/server";
 
 import { logger, env } from "#config/index.js";
 import { setupMiddleware } from "#middleware/index.js";
-import { typeDefs, resolvers } from "#graphql/index.js";
+import { apolloServer } from "./apollo.server.js";
 
 const { PORT, BACKEND_URL } = env;
 
 const app = express();
-
-const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  introspection: true,
-  allowBatchedHttpRequests: true,
-});
 
 (async function main() {
   await apolloServer.start();
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,5 +3,4 @@ export * from "./mail.utils.js";
 export * from "./otp.utils.js";
 export * from "./password.utils.js";
 export * from "./token.utils.js";
-export * from "./validations.utils.js";
 export * from "./verify.js";
