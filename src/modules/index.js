b3cab8949b05cd7fb58315367fe38485121f5c1d
Sharjeel Faiq <mrmalik610@gmail.com>
2025-11-11 23:51:41 +0500

    Add sub-category type definitions and resolvers to the main module

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,10 +5,12 @@ import { userTypeDefs, userResolvers } from "./user/index.js";
 import { authTypeDefs, authResolvers } from "./auth/index.js";
 import { emailTypeDefs, emailResolvers } from "./email/index.js";
 import { categoryTypeDefs, categoryResolvers } from "./category/index.js";
+import { subCategoryTypeDefs, subCategoryResolvers } from "./subCategory/index.js";
 
 export const typeDefs = mergeTypeDefs([
   authTypeDefs,
   categoryTypeDefs,
+  subCategoryTypeDefs,
   emailTypeDefs,
   otpTypeDefs,
   userTypeDefs,
@@ -17,6 +19,7 @@ export const typeDefs = mergeTypeDefs([
 export const resolvers = mergeResolvers([
   authResolvers,
   categoryResolvers,
+  subCategoryResolvers,
   emailResolvers,
   otpResolvers,
   userResolvers,
