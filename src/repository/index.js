fe948331f18ff3fbe24e1bbadef553674769f3a5
Sharjeel Faiq <mrmalik610@gmail.com>
2025-11-11 23:54:51 +0500

    Add subCategory repository methods to the main repository export

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,4 +1,5 @@
 import { categoryRepository } from "./category.repository.js";
+import { subCategoryRepository } from "./subCategory.repository.js";
 import { userRepository } from "./user.repository.js";
 import { otpRepository } from "./otp.repository.js";
 
@@ -7,21 +8,25 @@ export const repository = {
     ...categoryRepository.read,
     ...otpRepository.read,
     ...userRepository.read,
+    ...subCategoryRepository.read,
   },
 
   write: {
     ...categoryRepository.write,
     ...otpRepository.write,
     ...userRepository.write,
+    ...subCategoryRepository.write,
   },
 
   update: {
     ...categoryRepository.update,
     ...userRepository.update,
+    ...subCategoryRepository.update,
   },
 
   remove: {
     ...categoryRepository.remove,
     ...userRepository.remove,
+    ...subCategoryRepository.remove,
   },
 };
